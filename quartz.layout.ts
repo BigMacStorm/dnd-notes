43d7ed52481fd03220674fe362efdc4b7eba6c23
BigMacStorm <aaronbmwhitehead@gmail.com>
2025-04-03 02:51:08 -0700

    Quartz sync: Apr 3, 2025, 2:51 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,21 +42,6 @@ export const defaultContentPageLayout: PageLayout = {
       folderClickBehavior: "link", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
       folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
       useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
-      // omitted but shown later
-      sortFn: (a, b) => {
-        if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-          return a.displayName.localeCompare(b.displayName, undefined, {
-            numeric: true,
-            sensitivity: "base",
-          })
-        }
-     
-        if (!a.isFolder && b.isFolder) {
-          return 1
-        } else {
-          return -1
-        }
-      },
     }),
   ],
   right: [
